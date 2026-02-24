3db0691c07c3453a66d03bbc52a2e05d3fb14236
Hydro <darwinsumait38@gmail.com>
2026-02-24 14:15:53 +0800

    Auto Deploy: update site

---
+++ src/Landing.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="h-min-screen w-screen bg-white">
+        <h1>Test</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
