e576a0e216fbbf4bc31d3364636558c025a428ad
Hydro <darwinsumait38@gmail.com>
2026-02-24 14:36:50 +0800

    Auto Deploy: update site

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -5,8 +5,8 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="h-min-screen w-screen bg-white">
-        <h1>Test</h1>
+      <div className="h-min-screen w-full bg-white">
+        <h1 className="text-red-500">Test</h1>
       </div>
     </>
   );
