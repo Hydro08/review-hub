94d4873af0bbff4337759d677bbb747d6efa4c0e
Hydro <darwinsumait38@gmail.com>
2026-03-19 21:58:58 +0800

    Auto Deploy: update site

--- src/pages/Flashcard.jsx
+++ src/pages/Flashcard.jsx
@@ -129,8 +129,9 @@ function FlashcardPage() {
         <div className="flex w-full items-center justify-center gap-2">
           <button className="primary-border h-12 w-48">Create Flashcard</button>
           <button className="primary-border h-12 w-44">Select Category</button>
-          <button className="primary-border h-12 w-24">...</button>
+          <button className="primary-border h-12 w-24">Refresh</button>
         </div>
+        
       </section>
     </main>
   );
